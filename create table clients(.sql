2473772af2846e4039855ec2219dfe2dd660ff2f
Yesica Giraldo <yagiraldot@ieee.org>
2026-01-14 18:09:44 -0500

    creacion de otra tabla

--- create table clients(.sql
+++ create table clients(.sql
@@ -25,9 +25,33 @@ create table if not exists bills(
     created_at timestamp not null default CURRENT_TIMESTAMP,
     updated_at timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     foreign key (client_id) references clients(client_id)
+on delete cascade
+on update cascade
 
-)
+);
 
 
-insert int bills(client_id, total) values(10,15.00);
+
+
+insert into bills(client_id, total) values(10,15.00);
+insert into clients(client_id, name, email) values(10, 'Yesica', 'yagiraldot@ieee.org');
+
+
+
+#### tabla nueva 
+
+
+create table if not exists bill_products (
+bill_product_id integer unisigned primary key auto_increment,
+bill_id integer not null,
+product_id integer unsigned not null,
+quantity integer not null default 1,
+    created_at timestamp not null default CURRENT_TIMESTAMP,
+    updated_at timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    foreign key (bill_id) references bills(bill_id)
+    on update cascade
+    on delete cascade,
+);
+
+
 
